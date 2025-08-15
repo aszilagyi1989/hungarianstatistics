f5ae320e11ab0708d14ff102ee74aeb5e9594e10
vl05953 <adam.szilagyi@ksh.hu>
2025-08-15 13:02:43 +0200

    Adding new first files

---
+++ hungarianstatistics/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, redirect
+# from django.contrib.auth.decorators import login_required
+# from allauth.account.forms import LoginForm, SignupForm
+from django.contrib import messages
+
+def homepage(request):
+    return render(request, 'home.html')
