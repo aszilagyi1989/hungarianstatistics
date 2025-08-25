c6ac31b71fd9d536bd7b544992309da3f2da960e
vl05953 <adam.szilagyi@ksh.hu>
2025-08-25 13:43:04 +0200

    delete recaptcha 3

--- users/views.py
+++ users/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from allauth.account.forms import LoginForm, SignupForm
 from django.contrib import messages
-from .forms import ContactForm
 
 
 def signup_view(request):
@@ -28,17 +27,3 @@ def landing(request):
 @login_required
 def home(request):
     return render(request, 'home.html', {'user': request.user})
-
-def contact(request):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        
-        if form.is_valid():
-            return HttpResponse("Yes, you are a human.")
-        else:
-            return HttpResponse("Bot suspected.")
-          
-    else:
-        form = ContactForm()
-        
-    return render(request, 'contact.html', {'form':form})
